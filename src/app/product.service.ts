e47081167a4b2c092f96e95cab93fe0ff7ff2222
aishatsalisu <ao.salisu@outlook.com>
2021-12-14 03:19:34 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,9 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operator/map';
-Http
+import { map } from 'rxjs/operators';
+
+import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ProductService {
@@ -9,9 +11,7 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
 
   getAlbum(id:number){
-    this._http.get(this._albumUrl).pipe(
-      
-    );
+    return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 
   constructor(private _http:Http) { }
