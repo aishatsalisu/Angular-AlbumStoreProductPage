29ee7ea2b49bcac4f5004ebf7040181c603db21b
aishatsalisu <ao.salisu@outlook.com>
2021-12-14 03:21:51 +0100

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'app/product.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product-description',
